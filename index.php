78f2a0adadda36b723eade27eede5068bbfd35d5
OpenShift guest <vid14114@spengergasse.at>
2013-03-01 05:30:22 -0500

    pear test

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
+    require_once 'HTML/Template/Sigma.php';
     echo "test";
 ?>
 
 <html>
 <head>
+    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
     <title>Login</title>
 </head>
 
 <body>
-    <h1>HTML test</h1>
+<h1>HTML test</h1>
 </body>
 </html>

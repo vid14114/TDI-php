1271972f1b3138266223af5c0bba5cad74c57234
Viktor Vidovic <vid14114@spengergasse.at>
2013-03-08 10:59:42 +0100

    twitter bootstrap through CDN

--- index.php
+++ index.php
@@ -5,11 +5,12 @@
 
 <html>
 <head>
-    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
     <title>Login</title>
 </head>
 
 <body>
 <h1>HTML test</h1>
 </body>
+<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
 </html>

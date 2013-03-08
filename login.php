107b00b65450b4940f10fbffece3d21463e2de0f
Viktor Vidovic <vid14114@spengergasse.at>
2013-03-08 11:15:32 +0100

    first login interface

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
+	<title>TDI-php Login</title>
+</head>
+
+<body>
+	<div class="row>
+	     <div class="span4"></div>
+	     <div class="span4">
+	     	  <form>
+			<fieldset>
+				<legend>Login</legend>
+				<input type="text" placeholder="Username">
+				<input type="password" placeholder="Password">
+				<button type="submit" class="btn">Login</button>
+			</fieldset>
+		  </form>
+	     </div>
+	     <div class="span4"></div>
+	</div>
+</body>
+</html>

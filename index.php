57f28470728497057f5beb6473e0022b7a902759
Viktor Vidovic <vid14114@spengergasse.at>
2013-03-08 12:12:46 +0100

    cleanup

--- index.php
+++ index.php
@@ -1,16 +1,3 @@
 <?php
-    require_once 'HTML/Template/Sigma.php';
-    echo "test";
+	require_once 'login.html';
 ?>
-
-<html>
-<head>
-    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
-    <title>Login</title>
-</head>
-
-<body>
-<h1>HTML test</h1>
-</body>
-<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/js/bootstrap.min.js"></script>
-</html>

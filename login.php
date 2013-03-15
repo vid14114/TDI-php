1aa13883a520b6b13b5b742cae627ca76401da8a
Viktor Vidovic <vid14114@spengergasse.at>
2013-03-15 10:27:28 +0100

    rework sigma includes

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
    require_once 'HTML/Template/Sigma.php';
 
    $tpl = new HTML_Template_Sigma('.');
-   $tpl->loadTemplateFile('layout.html');
+   $tpl->loadTemplateFile('login.html');
 
    $tpl->show();
 ?>

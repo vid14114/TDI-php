af89e7970d0dce66939c6e856a8db2997f461d8a
Viktor Vidovic <vid14114@spengergasse.at>
2013-03-22 11:04:34 +0100

    mdb2 test

--- eval.php
+++ eval.php
@@ -1,4 +1,23 @@
 <?php
+	require_once 'MDB2.php';
+
+	$dsn = array(
+	     'phptype' => 'mysqli',
+	     'username' => 'tdi-read',
+	     'password' => 'read-tdi',
+	     'hostspec' => 'vidovic.no-ip.org',
+	     'database' => 'tdi'
+);
+	$options = array(
+		 'debug' => 2,
+		 'portability' => MDB_PORTABILITY_ALL
+);
+
+	$mdb2 =& MDB2::connect($dsn, $options);
+	if(PEAR::isError($mdb2)) {
+			//log message
+	}	
+
 	$username=$_POST['username'];
 	$passwd=$_POST['password'];
 

e3b8daadea50d84b52a78e78c766441ea229fd7c
Viktor Vidovic <vid14114@spengergasse.at>
2013-04-19 03:33:13 -0700

    add new user to database

---
+++ register.php
@@ -0,0 +1,41 @@
+<?php
+    session_start();
+    require_once 'MDB2.php';
+    require_once 'Log.php';
+    $logger = Log::singleton('file', 'register.log', 'Log');
+    ini_set('display_errors', '0'); 
+    
+    $dsn = array(
+        'phptype' => 'mysqli',
+        'username' => 'tdi-write',
+        'password' => 'write-tdi',
+        'hostspec' => 'localhost',
+        'port' => '3336',
+        'database' => 'tdi'
+    );
+    
+    $options = array(
+        'debug' => 2,
+        'result_buffering' => false
+    );
+    
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    $email=$_POST['email'];
+    
+    $mdb2 =& MDB2::factory($dsn, $options);
+    if(PEAR::isError($mdb2)) {
+        $logger->log($mdb2->getMessage(), PEAR_LOG_ERR);
+        die($mdb2->getMessage());
+    }
+    
+    $sql = "INSERT INTO u_users (u_accountname, u_password, u_email) VALUES (".$username.",".$password.",".$email.")";
+    $result =& $mdb2->exec($sql);
+    if(PEAR::isError($result)) {
+        $logger->log($result->getMessage(), PEAR_LOG_ERR);
+        die($result->getMessage());
+    }
+    else
+        echo "Succeeded";
+    
+?>

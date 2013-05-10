ab07f293d8cd29a011954ee0526aa4310c1f5899
Viktor Vidovic <vid14114@spengergasse.at>
2013-05-10 03:23:57 -0700

    remove more messages

--- upload-config.php
+++ upload-config.php
@@ -4,8 +4,6 @@
     require_once 'Log.php';
     $logger = Log::singleton('file', 'upload.log', 'Log');
     
-    print_r($_FILES);
-    
     if($_FILES['config_file']['error'] > 0)
         $logger->log($_FILES['config_file']['error']);
     else

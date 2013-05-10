1b2f8bb3baab20a4b1c630bd9197e791dbd9e325
Viktor Vidovic <vid14114@spengergasse.at>
2013-05-10 02:09:32 -0700

    redirect user to home, if he isn'T loged in

--- navigation.php
+++ navigation.php
@@ -1,8 +1,12 @@
 <?php
 require_once 'HTML/Template/Sigma.php';
-$filename = $_GET['filename'];
-
+if(!isset($_SESSION['user']))
+    $tpl->loadTemplateFile('login.html');
+else
+{
+    $filename = $_GET['filename'];
     $tpl = new HTML_Template_Sigma('.');
     $tpl->loadTemplateFile($filename.'.html');
     $tpl->show();
+}
 ?>

cf915e95b72a71db0ee4067ae0fb2f171b2d55e4
has14030 <has14030@spengergasse.at>
2013-05-24 10:59:20 +0200

    added a main.php

---
+++ main.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+require_once 'HTML/Template/Sigma.php';
+$tpl = new HTML_Template_Sigma('.');
+if(isset($_GET['logout']) && $_GET['logout'])
+    unset($_SESSION['user']);
+if(!isset($_SESSION['user']))
+    $tpl->loadTemplateFile('login.html');
+else
+    $tpl->loadTemplateFile('main.html');
+$tpl->show();
+?>

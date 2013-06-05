b81f02037bd2dbf1a0a65feb0014d8095760d5cd
Abideen Bamgbala <bam14053@spengergasse.at>
2013-06-05 10:31:36 +0200

    Corrected the shorttags

--- passwordStrength.js
+++ passwordStrength.js
@@ -25,7 +25,3 @@ $(function()
         }
     });
 });
-/*<div class="bar bar-danger" style="width: 35%;">Weak</div>
- <div class="bar bar-warning" style="width: 35%;">Medium</div>
- <div class="bar bar-success" style="width: 30%;">Strong</div>
- */

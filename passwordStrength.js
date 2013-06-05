5f785d6fccd95a5d240a341fe60b93cacc980090
Abideen Bamgbala <bam14053@spengergasse.at>
2013-06-05 10:23:42 +0200

    Finished the password strength

--- passwordStrength.js
+++ passwordStrength.js
@@ -10,7 +10,19 @@ $(function()
 {
     var password = $('#password');
     password.bind('input',function(e){
-        console.log(password.val());
+        $('.progress .bar').addClass('hidden');
+        if(password.val().length > 5){
+            $('.progress .bar:eq(0)').removeClass('hidden');
+            console.log("Im here 2");
+            if(password.val().length > 7 && /[^a-z]/.exec(password.val())[0] != null){
+                console.log("Im here 3");
+                $('.progress .bar:eq(1)').removeClass('hidden');
+            }
+            if(password.val().length > 10 && /[0-9]/.exec(password.val())[0] != null){
+                $('.progress .bar:eq(2)').removeClass('hidden');
+                console.log("Im here 4");
+            }
+        }
     });
 });
 /*<div class="bar bar-danger" style="width: 35%;">Weak</div>
